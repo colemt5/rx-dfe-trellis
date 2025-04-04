8af6a0d7c23694b08c430335b7ecedb0900436e2
Class Account <colemt5@gmail.com>
2025-04-03 22:35:13 -0700

    add in preliminary files

---
+++ src/main/scala/pdfd/MUXU.scala
@@ -0,0 +1,19 @@
+package pdfd
+
+import chisel3._
+import chisel3.util.log2Ceil
+
+/** Top module for the PDFD (Parallel Decision Feedback Decoder) project.
+  *
+  * todo add a description of the project.
+  */
+class MUXU() // todo add parameters 
+    extends Module {
+  val io = IO(new Bundle {
+    // todo add port declarations
+  })
+  
+  // todo add module implementation
+
+
+}
---
+++ src/test/scala/pdfd/PDFDSpec.scala
@@ -0,0 +1,24 @@
+
+package pdfd
+
+import chisel3._
+import chisel3.util.log2Ceil
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+
+// todo instantiate the PDFD mudule here 
+
+/** This is a trivial example of how to run this Specification From within sbt
+  * use:
+  * {{{
+  * testOnly pdfd.PDFDSpec
+  * }}}
+  * From a terminal shell use:
+  * {{{
+  * sbt 'testOnly pdfd.PDFDSpec'
+  * }}}
+  */
+class PDFDSpec extends AnyFreeSpec with ChiselScalatestTester {
+
+  // todo add test cases here
+}
