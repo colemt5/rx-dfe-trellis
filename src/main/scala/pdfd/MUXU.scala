9c65ea919d907bb231f8d44985175f75fae65fa6
Class Account <colemt5@gmail.com>
2025-04-06 20:24:39 -0700

    expand initial muxu design to include all channels

--- src/main/scala/pdfd/MUXU.scala
+++ src/main/scala/pdfd/MUXU.scala
@@ -3,45 +3,71 @@ package pdfd
 import chisel3._
 import chisel3.util.log2Ceil
 
-/** MUXU module that take all possible branch metrics and selects one of them
-  * based on the input selection signal.
+/** SymMux helper module that takes in two sets of branch metrics and selects one
+  * based on the input selection signal. Takes in all channels
   * 
   * @param bitWidth the bit width of the input and output signals
   */
-class MUXU(bitWidth: Int) // todo add parameters 
-    extends Module {
-  val io = IO(new Bundle {
-    val sel = Input(UInt(3.W))
-    val inBranchMetricsA = Input(Vec(5, SInt(bitWidth.W))) // todo need to verify if int or fixed point
-    val inBranchMetricsB = Input(Vec(5, SInt(bitWidth.W))) // todo need to verify if int or fixed point
-    val branchMetricA = Output(SInt(bitWidth.W)) // todo need to verify if int or fixed point
-    val branchMetricB = Output(SInt(bitWidth.W)) // todo need to verify if int or fixed point
-  })
-  
-  // Default to zero
-  io.branchMetricA := 0.S
-  io.branchMetricB := 0.S
 
-  switch(io.sel) {
+class SymMux(bitWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    val symSelect = Input(UInt(3.W))
+    val symMetricsA = Input(Vec(5, SInt(bitWidth.W)))
+    val symMetricsB = Input(Vec(5, SInt(bitWidth.W)))
+    val brMetricA = Output(SInt(bitWidth.W))
+    val brMetricB = Output(SInt(bitWidth.W))
+  })
+
+  io.brMetricA := 0.S
+  io.brMetricB := 0.S
+
+  switch(io.symSelect) {
     is(0.U) {
-      io.branchMetricA := io.inBranchMetricsA(0)
-      io.branchMetricB := io.inBranchMetricsB(0)
+      io.brMetricA := io.symMetricsA(0)
+      io.brMetricB := io.symMetricsB(0)
     }
     is(1.U) {
-      io.branchMetricA := io.inBranchMetricsA(1)
-      io.branchMetricB := io.inBranchMetricsB(1)
+      io.brMetricA := io.symMetricsA(1)
+      io.brMetricB := io.symMetricsB(1)
     }
     is(2.U) {
-      io.branchMetricA := io.inBranchMetricsA(2)
-      io.branchMetricB := io.inBranchMetricsB(2)
+      io.brMetricA := io.symMetricsA(2)
+      io.brMetricB := io.symMetricsB(2)
     }
     is(3.U) {
-      io.branchMetricA := io.inBranchMetricsA(3)
-      io.branchMetricB := io.inBranchMetricsB(3)
+      io.brMetricA := io.symMetricsA(3)
+      io.brMetricB := io.symMetricsB(3)
     }
     is(4.U) {
-      io.branchMetricA := io.inBranchMetricsA(4)
-      io.branchMetricB := io.inBranchMetricsB(4)
+      io.brMetricA := io.brMetricsA(4)
+      io.brMetricB := io.brMetricsB(4)
     }
   }
+}
+
+/** MUXU module that take all possible branch metrics and selects one of them
+  * based on the input selection signal. Takes in all channels
+  * 
+  * @param bitWidth the bit width of the input and output signals
+  */
+
+class MUXU(bitWidth: Int)
+    extends Module {
+  val io = IO(new Bundle {
+    val symSelects = Input(Vec(4, UInt(3.W)))
+    val brMetricsA = Input(Vec(4, Vec(5, SInt(bitWidth.W))))
+    val brMetricsB = Input(Vec(4, Vec(5, SInt(bitWidth.W))))
+    val chanBrMetricsA = Output(Vec(4, SInt(bitWidth.W)))
+    val chanBrMetricsB = Output(Vec(4, SInt(bitWidth.W)))
+})
+
+val mux = Seq.fill(4)(Module(new SymMux(bitWidth)))
+
+for (i <- 0 until 4) {
+  mux(i).io.symSelect := io.symSelects(i)
+  mux(i).io.brMetricsA := io.brMetricsA(i)
+  mux(i).io.brMetricsB := io.brMetricsB(i)
+  io.brMetricA(i) := mux(i).io.brMetricA
+  io.brMetricB(i) := mux(i).io.brMetricB
+}
 }
